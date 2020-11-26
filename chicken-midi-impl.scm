97fc706fa4f47b6dad668597831d32a83010b668
Cthulhu Ftaghn <cthulhu@ryleh.example.com>
2020-11-25 18:08:06 -0800

    created midi-read-from-port and called it

--- chicken-midi-impl.scm
+++ chicken-midi-impl.scm
@@ -1,2 +1,8 @@
+(define (midi-read-from-port port)
+  (let ((file (read-u8vector port)))
+    (print file)))
+
 (define (midi-open-file filename mode)
-  '())
+  (call-with-input-file filename
+    (lambda (port)
+      (midi-read-from-port port))))
